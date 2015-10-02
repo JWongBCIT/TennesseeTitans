f80611c823eb0dfece72485b625c622795facc8f
spero <slangaditis94@hotmail.com>
2015-10-02 13:57:41 -0700

    Added the roster page + unformatted content along with the roster controller.
    Also created the base controller, added autoload configurations, as well as added a helper class.

---
+++ application/views/roster.php
@@ -0,0 +1,83 @@
+<p>
+Active
+
+#	NAME	POS.	HT.	WT.	AGE	EXP.	COLLEGE
+26	Andrews, Antonio	RB	5-10	225	23	2	Western Kentucky
+51	Bass, David	LB	6-4	256	25	3	Missouri Western State
+76	Bell, Byron	T	6-5	340	26	5	New Mexico
+95	Blackson, Angelo	DT	6-4	318	22	R	Auburn
+48	Brinkley, Beau	LS	6-4	248	25	4	Missouri
+55	Brown, Zach	LB	6-1	248	25	4	North Carolina
+99	Casey, Jurrell	DT	6-1	305	25	5	USC
+85	Coffman, Chase	TE	6-6	250	28	6	Missouri
+29	Cox, Perrish	CB	6-0	190	28	5	Oklahoma State
+83	Douglas, Harry	WR	6-0	183	31	8	Louisville
+80	Fasano, Anthony	TE	6-4	255	31	10	Notre Dame
+45	Fowler, Jalston	FB	5-11	254	25	R	Alabama
+69	Gallik, Andy	C	6-2	306	25	R	Boston College
+17	Green-Beckham, Dorial	WR	6-5	237	22	R	Missouri
+33	Griffin, Michael	S	6-0	215	30	9	Texas
+94	Hill, Sammie	NT	6-4	328	28	7	Stillman
+28	Huff, Marqueston	DB	5-11	196	23	2	Wyoming
+15	Hunter, Justin	WR	6-4	203	24	3	Tennessee
+52	Johnson, Steven	MLB	6-1	237	27	4	Kansas
+90	Jones, DaQuan	DL	6-4	322	23	2	Penn State
+6	Kern, Brett	P	6-2	214	29	8	Toledo
+97	Klug, Karl	DL	6-3	278	27	5	Iowa
+77	Lewan, Taylor	T	6-7	309	24	2	Michigan
+8	Mariota, Marcus	QB	6-4	222	21	R	Oregon
+93	Martin, Mike	DL	6-1	298	25	4	Michigan
+22	McCluster, Dexter	RB	5-8	170	27	6	Mississippi
+30	McCourty, Jason	CB	6-0	193	28	7	Rutgers
+79	Meredith, Jamon	T	6-5	312	29	7	South Carolina
+7	Mettenberger, Zach	QB	6-5	224	24	2	LSU
+91	Morgan, Derrick	OLB	6-3	261	26	6	Georgia Tech
+53	Mount, Deiontrez	OLB	6-5	249	22	R	Louisville
+98	Orakpo, Brian	OLB	6-4	257	29	7	Texas
+92	Pitoitua, Ropati	DE	6-8	298	30	6	Washington State
+72	Poutasi, Jeremiah	T	6-5	335	21	R	Utah
+37	Riggs, Cody	CB	5-9	187	23	R	Notre Dame
+20	Sankey, Bishop	RB	5-10	209	23	2	Washington
+62	Schwenke, Brian	C	6-3	318	24	3	California
+21	Searcy, Da'Norris	S	5-11	207	26	5	North Carolina
+24	Sensabaugh, Coty	CB	5-11	187	26	4	Clemson
+60	Spain, Quinton	G	6-4	330	24	R	West Virginia
+39	Stafford, Daimion	S	6-1	218	24	3	Nebraska
+88	Stevens, Craig	TE	6-3	263	31	8	California
+4	Succop, Ryan	K	6-2	218	29	7	South Carolina
+89	Supernaw, Phillip	TE	6-5	248	25	2	Ouachita Baptist
+82	Walker, Delanie	TE	6-0	248	31	10	Central Missouri
+70	Warmack, Chance	G	6-2	323	24	3	Alabama
+35	West, Terrance	RB	5-10	225	24	2	Towson
+12	Whitehurst, Charlie	QB	6-5	226	33	10	Clemson
+54	Williamson, Avery	LB	6-1	246	23	2	Kentucky
+96	Woods, Al	DL	6-4	307	28	6	LSU
+59	Woodyard, Wesley	LB	6-0	233	29	8	Kentucky
+25	Wreh-Wilson, Blidi	CB	6-1	198	25	3	Connecticut
+13	Wright, Kendall	WR	5-10	191	25	4	Baylor
+
+Reserve/Injured
+
+#	NAME	POS.	HT.	WT.	AGE	EXP.	COLLEGE
+42	Dickerson, Dorin	TE	6-1	226	27	4	Pittsburgh
+32	Harris, Brandon	CB	5-10	189	25	5	Miami (Fla.)
+46	Neighbors, Connor	FB	5-10	235	23	R	LSU
+46	Riley, Curtis	CB	6-0	190	23	R	Fresno State
+Practice Squad
+#	NAME	POS.	HT.	WT.	AGE	EXP.	COLLEGE
+56	Ankrah, Jason	LB	6-3	262	24	1	Nebraska
+31	Aubrey, Josh	S	5-10	207	24	3	Stephen F. Austin
+61	Farrell, Dillon	C/G	6-5	303	25	2	New Mexico
+32	Fluellen, David	RB	5-11	224	23	1	Toledo
+66	Matias, Josue	G	6-5	309	22	R	Florida State
+16	McBride, Tre	WR	6-0	210	22	R	William & Mary
+75	Poehls, William	T	6-8	334	23	1	Montana
+81	Richardson, Rico	WR	6-1	185	24	1	Jackson State
+57	Staples, Justin	LB	6-4	245	25	2	Illinois
+38	Webb, B.W.	CB	5-11	190	25	3	William & Mary
+
+Reserve/Designated to Return
+
+#	NAME	POS.	HT.	WT.	AGE	EXP.	COLLEGE
+23	Cobb, David	RB	5-11	229	22	R	Minnesota
+</p>
