7a366f56a7b8f9a16f1fb58d57e95002db54529a
jason <jason@jason-S400CA>
2015-11-15 15:29:41 -0800

    Changed DB query to grab name, city and img filename and displayed them in view

--- application/models/Leagues.php
+++ application/models/Leagues.php
@@ -1,31 +1,11 @@
 <?php
 
-/* This class repersents a team in the league */
-
-/**
- * @author adamh
- */
-class Team {
-
-    function __construct($name, $div, $key) {
-        $this->teamName = $name;
-        $this->div = $div;
-        $this->key = $key;
-    }
-
-    public $key = "";
-    public $teamName = "";
-    public $div = "";
-
-    
-    
-}
 
 /**
  * This is the league model. It repersents all the teams in the league 
  * with affiliated info.
  *
- * @author adamh
+ * @author adamh & jasonw
  */
 class Leagues extends CI_Model {
 
@@ -35,7 +15,7 @@ class Leagues extends CI_Model {
     }
 
     public function getByDivision($div) {
-        $this->db->select('name');
+        $this->db->select('name , city, filename');
         $this->db->where('division', $div);
         return $this->db->get('league')->result_array();
     }
